85e5bee0fea2a04840a7e926277cf68d9c14102f
Itamar Silva <itamarsilvacc@gmail.com>
2023-04-20 18:26:36 -0300

    Corrigindo dependência jquery do Bootstrap

--- functions.php
+++ functions.php
@@ -145,7 +145,7 @@ function tema_teste_dev_scripts() {
 
 	wp_enqueue_script( 'tema-teste-dev-popper', 'https://unpkg.com/@popperjs/core@2', array(), _S_VERSION, true );
 
-	wp_enqueue_script( 'tema-teste-dev-bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'tema-teste-dev-bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
