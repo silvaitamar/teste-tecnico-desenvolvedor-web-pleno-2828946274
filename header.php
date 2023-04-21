ab87ad91ded9082a7a822875c9a5d74600bafcba
Itamar Silva <itamarsilvacc@gmail.com>
2023-04-20 22:53:52 -0300

    Criando e estilizando cabeçalho do site

--- header.php
+++ header.php
@@ -25,35 +25,42 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'tema-teste-dev' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+	<header id="masthead" class="site-header mb-4 border-bottom">
+		<div class="site-header-inner container-fluid d-flex flex-wrap align-items-center justify-content-center justify-content-md-between">
+			<div class="site-branding col-md-3 mb-2 mb-md-0">
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$tema_teste_dev_description = get_bloginfo( 'description', 'display' );
-			if ( $tema_teste_dev_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $tema_teste_dev_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+				the_custom_logo();
+				if ( is_front_page() && is_home() ) :
+					?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php
+				else :
+					?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+				endif;
+				$tema_teste_dev_description = get_bloginfo( 'description', 'display' );
+				if ( $tema_teste_dev_description || is_customize_preview() ) :
+					?>
+					<p class="site-description"><?php echo $tema_teste_dev_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+				<?php endif; ?>
+			</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'tema-teste-dev' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+			<nav id="site-navigation" class="main-navigation col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'tema-teste-dev' ); ?></button>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_class'	 => 'nav',
+						'menu_id'        => 'primary-menu',
+					)
+				);
+				?>
+			</nav><!-- #site-navigation -->
+
+			<div class="col-md-3 text-end">
+				<button type="button" class="btn btn-outline-primary me-2">Login</button>
+			</div>
+		</div>
 	</header><!-- #masthead -->
