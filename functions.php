f8144b07a169d437e259876a1391af139e36b9dc
Itamar Silva <itamarsilvacc@gmail.com>
2023-04-22 02:42:15 -0300

    Corrigindo carregamento de scripts JavaScript

--- functions.php
+++ functions.php
@@ -145,11 +145,11 @@ function tema_teste_dev_scripts() {
 	wp_enqueue_style( 'tema-teste-dev-style', get_template_directory_uri() . '/dist/css/style.css', array(), _S_VERSION );
 	wp_style_add_data( 'tema-teste-dev-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'tema-teste-dev-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	//wp_enqueue_script( 'tema-teste-dev-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), _S_VERSION, true );
 
 	wp_enqueue_script( 'tema-teste-dev-popper', 'https://unpkg.com/@popperjs/core@2', array(), _S_VERSION, true );
 
-	wp_enqueue_script( 'tema-teste-dev-bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), _S_VERSION, true );
+	wp_enqueue_script( 'tema-teste-dev-bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array( 'jquery' ), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
