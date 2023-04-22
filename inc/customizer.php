35e493daf58f86dac5a7342b61feda309fe81658
Itamar Silva <itamarsilvacc@gmail.com>
2023-04-22 02:26:23 -0300

    Adicionando opções para links de redes sociais no Customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -31,6 +31,62 @@ function tema_teste_dev_customize_register( $wp_customize ) {
 			)
 		);
 	}
+
+	// Add new section for social media links
+	$wp_customize->add_section( 'tema_teste_dev_social_links_section', array(
+		'title'    => __( 'Social Media Links', 'tema-teste-dev' ),
+		'priority' => 30,
+	) );
+
+	// Add settings for social media links
+	$wp_customize->add_setting( 'tema_teste_dev_instagram_link', array(
+		'default'           => '',
+		'sanitize_callback' => 'esc_url_raw',
+	) );
+
+	$wp_customize->add_setting( 'tema_teste_dev_linkedin_link', array(
+		'default'           => '',
+		'sanitize_callback' => 'esc_url_raw',
+	) );
+
+	$wp_customize->add_setting( 'tema_teste_dev_whatsapp_link', array(
+		'default'           => '',
+		'sanitize_callback' => 'esc_url_raw',
+	) );
+
+	$wp_customize->add_setting( 'tema_teste_dev_youtube_link', array(
+		'default'           => '',
+		'sanitize_callback' => 'esc_url_raw',
+	) );
+
+	// Add controls for social media links
+	$wp_customize->add_control( 'tema_teste_dev_instagram_link_control', array(
+		'label'    => __( 'Instagram Link', 'tema-teste-dev' ),
+		'section'  => 'tema_teste_dev_social_links_section',
+		'settings' => 'tema_teste_dev_instagram_link',
+		'type'     => 'text',
+	) );
+
+	$wp_customize->add_control( 'tema_teste_dev_linkedin_link_control', array(
+			'label'    => __( 'LinkedIn Link', 'tema-teste-dev' ),
+			'section'  => 'tema_teste_dev_social_links_section',
+			'settings' => 'tema_teste_dev_linkedin_link',
+			'type'     => 'text',
+	) );
+
+	$wp_customize->add_control( 'tema_teste_dev_whatsapp_link_control', array(
+		'label'    => __( 'WhatsApp Link', 'tema-teste-dev' ),
+		'section'  => 'tema_teste_dev_social_links_section',
+		'settings' => 'tema_teste_dev_whatsapp_link',
+		'type'     => 'text',
+	) );
+
+	$wp_customize->add_control( 'tema_teste_dev_youtube_link_control', array(
+		'label'    => __( 'YouTube Link', 'tema-teste-dev' ),
+		'section'  => 'tema_teste_dev_social_links_section',
+		'settings' => 'tema_teste_dev_youtube_link',
+		'type'     => 'text',
+	) );
 }
 add_action( 'customize_register', 'tema_teste_dev_customize_register' );
 
